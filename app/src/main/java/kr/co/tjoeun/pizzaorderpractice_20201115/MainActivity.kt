b94903fad2bd9975733b2d0dcc2fd8cddcbf612c
조경진 <kj_cho@idealidea.co.kr>
2020-11-15 15:53:42 +0900

    어댑터 / 뷰페이저 연결 작업

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/MainActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.pizzaorderpractice_20201115
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.pizzaorderpractice_20201115.adapters.MyViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+    lateinit var myViewPagerAdapter : MyViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        myViewPagerAdapter = MyViewPagerAdapter(supportFragmentManager)
+        myViewPager.adapter = myViewPagerAdapter
 
     }
 
