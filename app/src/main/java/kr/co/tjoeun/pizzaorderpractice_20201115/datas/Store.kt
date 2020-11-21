f0b115d61133cd13ad4942ff49bd872686c2bc8c
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 12:28:49 +0900

    피자가게 리스트뷰 아이템 클릭 이벤트 처리  + Serializable로 가게정보 한꺼번에 첨부하기  + 상세정보 액티비티는 작업 필요한 상태

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/datas/Store.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/datas/Store.kt
@@ -1,7 +1,9 @@
 package kr.co.tjoeun.pizzaorderpractice_20201115.datas
 
+import java.io.Serializable
+
 class Store(
     val name: String,
     val phoneNum: String,
-    val logoURL: String) {
+    val logoURL: String) : Serializable {
 }
--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/fragments/PizzaStoreFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/fragments/PizzaStoreFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.pizzaorderpractice_20201115.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_pizza_store.*
 import kr.co.tjoeun.pizzaorderpractice_20201115.R
+import kr.co.tjoeun.pizzaorderpractice_20201115.ViewStoreDetailActivity
 import kr.co.tjoeun.pizzaorderpractice_20201115.adapters.StoreAdapter
 import kr.co.tjoeun.pizzaorderpractice_20201115.datas.Store
 
@@ -38,6 +40,17 @@ class PizzaStoreFragment : Fragment() {
         mStoreAdapter = StoreAdapter(context!!, R.layout.store_list_item, mPizzaStoreList)
         pizzaStoreListView.adapter = mStoreAdapter
 
+
+        pizzaStoreListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedStore = mPizzaStoreList[position]
+
+            val myIntent = Intent(context, ViewStoreDetailActivity::class.java)
+            myIntent.putExtra("store", clickedStore)
+            startActivity(myIntent)
+
+
+        }
 
     }
 
