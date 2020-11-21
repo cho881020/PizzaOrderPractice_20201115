ca3c925a2870510f1ab9acb7d8a4dde2d7809c17
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 12:47:23 +0900

    실제 데이터들 반영  - 데이터담는 변수는 멤버변수로 하는게 편하다. lateinit var 활용

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/ViewStoreDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/ViewStoreDetailActivity.kt
@@ -2,8 +2,13 @@ package kr.co.tjoeun.pizzaorderpractice_20201115
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import kr.co.tjoeun.pizzaorderpractice_20201115.datas.Store
 
 class ViewStoreDetailActivity : BaseActivity() {
+
+    lateinit var mStore: Store
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,14 @@ class ViewStoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//        이전 목록화면에서 보내준 데이터를 멤버변수에 저장
+        mStore = intent.getSerializableExtra("store") as Store
+
+//        mStore를 활용해서 실 데이터 반영
+        storeNameTxt.text = mStore.name
+        storePhoneNumTxt.text = mStore.phoneNum
+
+        Glide.with(mContext).load(mStore.logoURL).into(storeLogoImg)
 
     }
 
