1b11104cfc5ccd9a1ff89e417c55c14eb75b2196
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 14:12:21 +0900

    내 정보 화면 구조 변경

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.pizzaorderpractice_20201115.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +22,15 @@ class MyProfileFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 //        내 닉네임 변경 관련 기능 작성해야함
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        받아온 닉네임 반영 작업 필요
+
+
     }
 
 }
