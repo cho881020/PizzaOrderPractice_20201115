88dab0efe195f535af0fcc468ecc80a6a3c37cac
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 13:50:02 +0900

    TedPermission 활용 - 권한체크 후, 실제 전화 연결

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/ViewStoreDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package kr.co.tjoeun.pizzaorderpractice_20201115
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 import kr.co.tjoeun.pizzaorderpractice_20201115.datas.Store
 
@@ -19,6 +25,39 @@ class ViewStoreDetailActivity : BaseActivity() {
 
 
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+
+//            전화버튼이 눌리면 => TedPermission 라이브러리로
+//            권한 확인하고 => 상황별 대응
+
+//            Code : 상황별 대응 방법 (가이드북-object이용) 먼저 생성 => 가이드북을 들고, 권한 확인
+
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                    권한이 승인되었다면? => 실제 전화 연결 (ACTION_CALL)
+
+                    val myUri = Uri.parse("tel:${mStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    최종 통화 불가 일때 => 안내 토스트
+                    Toast.makeText(mContext, "권한이 거부되어 전화연결이 불가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+//            가이드북 들고 권한 확인
+            TedPermission.with(mContext)
+                    .setPermissionListener(permissionListener)
+                    .setDeniedMessage("[설정] 에서 권한을 허락하셔야 전화연결이 가능합니다.")
+                    .setPermissions(Manifest.permission.CALL_PHONE)
+                    .check()
+
+        }
 
     }
 
