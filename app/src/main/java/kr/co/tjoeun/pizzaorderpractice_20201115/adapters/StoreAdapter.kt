f6d86d171e8c0c68090e638f9cc03b32b5b986ee
조경진 <kj_cho@idealidea.co.kr>
2020-11-21 11:24:38 +0900

    가게 이름까지 실제 반영

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/adapters/StoreAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/adapters/StoreAdapter.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 import kr.co.tjoeun.pizzaorderpractice_20201115.R
 import kr.co.tjoeun.pizzaorderpractice_20201115.datas.Store
 
 class StoreAdapter(
     val mContext: Context,
     resId: Int,
-    val mList: ArrayList<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
+    val mList: List<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
 
 //    mContext, mList => m으로 시작 : 멤버변수임을 명시
 //    멤버변수 : 변수중, 클래스 내부의 어느곳(=어떤 함수)에서든 사용가능한 변수.
@@ -33,6 +35,17 @@ class StoreAdapter(
 
 //        실제로 뿌려주기 전 row에 데이터들을 집어넣고
 
+//        (1) row내부의 TextView / ImageView 등등 에게
+//        (2) ArrayList 안에 있는 데이터를 꺼내서 => (3) 문구 / src 등 반영
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
+
+//        현재 그려주려는 위치(position 변수)에 맞는 데이터 (mList에서) 꺼내서 => 변수에 저장
+
+        val storeData = mList[position]
+
+        nameTxt.text = storeData.name
 
 //        완성된 row가 리스트뷰에 뿌려질 결과물
         return row
